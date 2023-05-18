57ba01724d9a9322b0020722be81acb4f5c798c4
AlbertRossJoh <93578997+AlbertRossJoh@users.noreply.github.com>
2023-05-18 20:01:55 +0200

    bag added no doc

---
+++ src/fundamentals/bag.rs
@@ -0,0 +1,35 @@
+use std::collections::{LinkedList, binary_heap::Iter};
+
+
+
+pub struct Bag<T> { 
+    elements: LinkedList<T>,
+    size: usize,
+}
+
+impl<T> Bag<T> {
+
+    pub fn new() -> Bag<T>{
+        Bag{
+            elements: LinkedList::new(),
+            size: 0,
+        }
+    }
+
+    pub fn add(&mut self, item: T){
+        self.elements.push_front(item);
+        self.size += 1;
+    }
+
+    pub fn is_empty(&mut self) -> bool{
+        self.elements.is_empty()
+    }
+
+    pub fn size(&mut self) -> usize{
+        self.size
+    }
+
+    pub fn iterator(&mut self) -> std::collections::linked_list::Iter<'_, T>{
+        self.elements.iter()
+    }
+}
--- src/fundamentals/mod.rs
+++ src/fundamentals/mod.rs
@@ -1,3 +1,4 @@
 pub mod stack;
 pub mod queue;
+pub mod bag;
 pub mod binary_search;
